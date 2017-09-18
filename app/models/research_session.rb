bb6a88b74d8e06544dfeec1da0ba103fec98b694
Zac Tolley <zac@thetolleys.com>
2017-09-18 15:25:07 +0100

    Update model for researchers
    Moved from having the researcher and researcher other fields in the research session table to creating a one to many relationship to a table named Researchers.

--- app/models/research_session.rb
+++ app/models/research_session.rb
@@ -1,10 +1,8 @@
 class ResearchSession < ApplicationRecord
-  validates :researcher_name, presence: true,
-            if: -> (session) { session.reached_step?(:researcher) }
-  validates :researcher_email, presence: true,
-            if: -> (session) { session.reached_step?(:researcher) }
-  validates :researcher_email, format: /@/,
-    if: -> (session) { session.researcher_email.present? && session.reached_step?(:researcher) }
+  has_many :researchers
+
+  validates :researchers, presence: true,
+    if: -> (session) { session.reached_step?(:researcher) }
 
   validates :topic, presence: true,
     if: -> (session) { session.reached_step?(:topic) }
---
+++ app/models/researcher.rb
@@ -0,0 +1,7 @@
+class Researcher < ApplicationRecord
+  belongs_to :research_session
+
+  validates :researcher_name, presence: true
+  validates :researcher_email, presence: true
+  validates :researcher_email, format: /@/
+end
