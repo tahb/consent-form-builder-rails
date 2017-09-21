5e232b559c87a64e2dfbb9ed2d9838f3d81cb982
Russell Garner <rgarner@zephyros-systems.co.uk>
2017-09-21 14:53:47 +0100

    Make the research_session_specs pass
    - Remove a redundant presence check from researcher
    - Remove a redundant status set from the validation specs
    - Make the specs use the step_ convention for factory girl traits

--- app/models/researcher.rb
+++ app/models/researcher.rb
@@ -2,6 +2,5 @@ class Researcher < ApplicationRecord
   belongs_to :research_session
 
   validates :researcher_name, presence: true
-  validates :researcher_email, presence: true
   validates :researcher_email, format: /@/
 end
